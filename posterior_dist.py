db4fdcae589be89fd1584bacf9e85fa7e0f56d2a
Yeseul, Jeon <70432016+jeon9677@users.noreply.github.com>
2024-01-22 17:20:58 +0900

    Update posterior_dist.py

--- posterior_dist.py
+++ posterior_dist.py
@@ -25,10 +25,10 @@ if __name__ == '__main__':
 
     exp_dir, n_model = sys.argv[1], int(sys.argv[2])
     size = 500
-    trace_path = os.path.join(exp_dir, f'trace.ny')
+    trace_path = os.path.join(exp_dir, f'trace.npy')
     mc_beta = load_np(trace_path)
 
-    sigma_path = os.path.join(exp_dir, f'posterior_sigma.ny')
+    sigma_path = os.path.join(exp_dir, f'posterior_sigma.npy')
     mc_sigma = load_np(sigma_path)
     res = []
     n_m = mc_beta.shape[0]
@@ -43,20 +43,24 @@ if __name__ == '__main__':
 
     res1 = res_array[:,0,:]
     res2 = res_array[:,1,:]
-    res3 = res_array[:, 2, :]
+    # res3 = res_array[:, 2, :]
 
 
     b1 = res1.reshape(-1)
     b2 = res2.reshape(-1)
-    b3 = res3.reshape(-1)
+    # b3 = res3.reshape(-1)
 
     b1 = b1.reshape(b1.shape[0], 1)
     b2 = b2.reshape(b2.shape[0], 1)
-    b3 = b3.reshape(b3.shape[0], 1)
+    # b3 = b3.reshape(b3.shape[0], 1)
 
 
-    beta_dist_final = np.hstack([b1,b2,b3])
-    name_list = ["b1"] + ["b2"] + ["b3"]
+    beta_dist_final = np.hstack([b1,b2])
+    name_list = ["b1"] + ["b2"] 
+
+    # beta_dist_final = np.hstack([b1,b2,b3])
+    # name_list = ["b1"] + ["b2"] + ["b3"]
+    
     beta_dist_final1 = pd.DataFrame(beta_dist_final, columns=name_list)
     df = pd.DataFrame(beta_dist_final1, columns=name_list)
 
